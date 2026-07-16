bdcf9cca892728744089e355e3abd0d3cdef31b7
Your Name <you@example.com>
2026-07-16 14:36:49 +0530

    Create superuser during deployment

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput || true
