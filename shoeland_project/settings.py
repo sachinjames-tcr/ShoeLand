111e4e330c6b0eb127c1712aa1adc77478584fb6
Your Name <you@example.com>
2026-07-16 11:36:25 +0530

    updated settings

--- shoeland_project/settings.py
+++ shoeland_project/settings.py
@@ -40,13 +40,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+   
 ]
 
 ROOT_URLCONF = "shoeland_project.urls"
